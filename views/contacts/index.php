1850d771a394ea20a6d1a1bfb170d18553f260b4
IamDMC <daniel.czosz@gmx.com>
2026-03-17 09:49:55 +0100

    feat: add link to create new contact

--- views/contacts/index.php
+++ views/contacts/index.php
@@ -1,17 +1,21 @@
 <h2>Kontaktliste</h2>
 
+<a href="/contacts/create" style="display:inline-block; margin-bottom:10px;">
+    Neuer Kontakt
+</a>
+
 <ul>
     <?php foreach ($contacts as $contact): ?>
             <li>
-                    <?= htmlspecialchars($contact['first_name']) ?>
-                    <?= htmlspecialchars($contact['last_name']) ?>
+                <?= htmlspecialchars($contact['first_name']) ?>
+                <?= htmlspecialchars($contact['last_name']) ?>
 
-                    <a href="/contacts/edit?id=<?= $contact['id'] ?>">Edit</a>
+                <a href="/contacts/edit?id=<?= $contact['id'] ?>">Edit</a>
 
-                    <form method="POST" action="/contacts/delete" style="display:inline;">
-                            <input type="hidden" name="id" value="<?= $contact['id'] ?>">
-                            <button>Löschen</button>
-                    </form>
+                <form method="POST" action="/contacts/delete" style="display:inline;">
+                        <input type="hidden" name="id" value="<?= $contact['id'] ?>">
+                        <button>Löschen</button>
+                </form>
             </li>
     <?php endforeach; ?>
 </ul>
