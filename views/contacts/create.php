e104c689c8cd116c3397b985f0946ff8c125c983
IamDMC <daniel.czosz@gmx.com>
2026-03-17 09:45:35 +0100

    feat: add basic validation for create and update forms

--- views/contacts/create.php
+++ views/contacts/create.php
@@ -1,5 +1,13 @@
 <h2>Kontakt erstellen</h2>
 
+<?php if (!empty($errors)): ?>
+    <ul style="color:red;">
+        <?php foreach ($errors as $error): ?>
+            <li><?= htmlspecialchars($error) ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
 <form method="POST" action="/contacts">
     <input name="first_name" placeholder="Vorname">
     <input name="last_name" placeholder="Nachname">
